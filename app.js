335b91604123d13c7c3d111bc051e07403f54247
Q-Mart <qumarthjash@gmail.com>
2016-04-08 14:06:03 +0100

    Basic app and also switched to npm

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var app = angular.module("pizza-time",[]);
 
-app.controller('mainCtrl', function($scope){
-  $scope.timeUntilPizza = 'hello world';
+app.controller('mainCtrl', function($scope, $interval){
+  var isDateTimeSet = false;
+  var isCountdownNotAtZero = true;
+  var timeUntilPizza;
+
+  $scope.message = 'Please choose a time';
+
+  var tick = function() {
+    if (isDateTimeSet && isCountdownNotAtZero) {
+      timeUntilPizza = $scope.arrivalDateTime - Date.now();
+      isCountdownNotAtZero = !(timeUntilPizza <= 0);
+
+      $scope.message = timeUntilPizza;
+    } else if (!isCountdownNotAtZero && isDateTimeSet) {
+      $scope.message = 'Pizza should be here';
+    }
+  }
+
+  $scope.setArrival = function(){
+    isDateTimeSet = true;
+    timeUntilPizza = $scope.arrivalDateTime - Date.now();
+  };
+
+  $interval(tick, 1000);
 });
